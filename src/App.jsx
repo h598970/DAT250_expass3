9ad9af3073062ac1955af05e3a28390f047da044
Andreas Øihaugen <89156396+h598970@users.noreply.github.com>
2024-09-15 23:03:58 +0200

    Added expass note

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import CreatePollComponent from "./components/poll.jsx";
 import VoteComponent from "./components/vote.jsx";
